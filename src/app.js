975920df508aae42f998f2c44e510cd4f386fdde
singhakshay29 <akshay2898.as@gmail.com>
2023-11-12 15:24:36 +0530

    Updates

--- src/app.js
+++ src/app.js
@@ -1,16 +1,41 @@
 const fs = require("fs");
 const express = require("express");
+const { error } = require("console");
 const app = express();
 
 // Importing products from products.json file
-const userDetails = JSON.parse(
-  fs.readFileSync(`${__dirname}/data/userDetails.json`)
-);
+const filepath = `${__dirname}/data/userDetails.json`;
+const userDetails = JSON.parse(fs.readFileSync(filepath));
 
 //Middlewares
 app.use(express.json());
 
 // Write POST endpoint for registering new user
+app.post("/api/v1/details/", (req, res) => {
+  const { name, mail, number } = req.body;
+  const newId = userDetails[userDetails.length - 1].id;
+  const newUser = {
+    name: name,
+    mail: mail,
+    number: number,
+    id: newId,
+  };
+
+  userDetails.push(newUser);
+  fs.writeFile(filepath, JSON.stringify(userDetails), (error) => {
+    if (error) {
+      res.status(400).json({ message: "There is error" });
+    } else {
+      res.status(201).json({
+        status: "Success",
+        message: "User registered successfully",
+        data: {
+          newProduct: newUser,
+        },
+      });
+    }
+  });
+});
 
 // GET endpoint for sending the details of users
 app.get("/api/v1/details", (req, res) => {
