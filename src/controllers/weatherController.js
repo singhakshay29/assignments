0fb5908fbff44fb5d0aa66f9d9272d0c0829ae37
singhakshay29 <akshay2898.as@gmail.com>
2023-11-17 02:01:15 +0530

    Updates

--- src/controllers/weatherController.js
+++ src/controllers/weatherController.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
+const fs = require("fs");
 
 async function getDataFromDatabase() {
   return new Promise((resolve, reject) => {
-    fs.readFile('src/data/data.json', (err, data) => {
+    fs.readFile("src/data/data.json", (err, data) => {
       if (err) {
         reject(err);
       } else {
@@ -15,7 +15,7 @@ async function getDataFromDatabase() {
 async function saveDataToDatabase(data) {
   return new Promise((resolve, reject) => {
     const jsonData = JSON.stringify(data);
-    fs.writeFile('src/data/data.json', jsonData, (err) => {
+    fs.writeFile("src/data/data.json", jsonData, (err) => {
       if (err) {
         reject(err);
       } else {
@@ -27,10 +27,19 @@ async function saveDataToDatabase(data) {
 
 // Level 1: Get City Weather Data by Name
 async function getWeatherDataByName(cityName) {
-  
+  return new Promise((resolve, reject) => {
+    fs.readFile("src/data/data.json", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const storedata = JSON.parse(data);
+        const requiredData = storedata.find((item) => item.city === cityName);
+        resolve(requiredData);
+      }
+    });
+  });
 }
 
-
 module.exports = {
-  getWeatherDataByName
+  getWeatherDataByName,
 };
