ad721dfc43e4ee2d73eb896e6521e7f3af1d31b6
singhakshay29 <akshay2898.as@gmail.com>
2023-11-17 01:44:01 +0530

    Changes

--- src/app.js
+++ src/app.js
@@ -1,21 +1,44 @@
-const fs = require('fs');
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
 const app = express();
 
-
 // Importing products from products.json file
-const products = JSON.parse(
-    fs.readFileSync(`${__dirname}/data/product.json`)
-);
-
+const products = JSON.parse(fs.readFileSync(`${__dirname}/data/product.json`));
 
 // Middlewares
 app.use(express.json());
 
 // Write PATCH endpoint to buy a product for the client here
 // Endpoint /api/v1/products/:id
+app.patch("/api/v1/products/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    const productId = +id;
+    const isPresent = products.find((item) => item.id === productId);
 
-
-
+    if (isPresent) {
+      const quantity = isPresent.quantity;
+      if (quantity < 1) {
+        res.status(200).json({
+          status: "success",
+          message: "Product" + id + ", Out of stock!",
+        });
+      } else {
+        res.status(200).json({
+          status: "success",
+          message: `Thank you for purchasing Product ${id}`,
+          product: isPresent,
+        });
+      }
+    } else {
+      res.status(404).json({
+        status: "failed",
+        message: "Product not found!",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ status: "error", message: "Internal server error" });
+  }
+});
 
 module.exports = app;
