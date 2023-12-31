912bc7bdb7ef14254488c0904267cf21d510a770
Akshay Singh <akshay@akshays-mac.local>
2023-12-31 09:20:50 +0530

    updates

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const productController = require('../controllers/productController');
+const productController = require("../controllers/productController");
 
-router.post('/', productController.createProduct);
+router.post("/", productController.createProduct);
 
-router.get('/product/', productController.getProducts);
+router.get("/product", productController.getProducts);
 
-router.post('/product/buy/:productId', productController.buyProduct);
+router.post("/product/buy/:productId", productController.buyProduct);
 
 module.exports = router;
